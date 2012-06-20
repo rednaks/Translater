1ceea32962588c220470d0fcf4bc03873b1e10a2
Zaafouri Amine <zaafouri.amine@gmail.com>
2012-06-20 17:18:51 +0100

    add curl + static translation of salut

--- src/functions.c
+++ src/functions.c
@@ -2,6 +2,18 @@
 
 void translate(char *s) {
 
-	printf("le texte est :%s ",s);
+	CURL *handle=curl_easy_init();
 
+	if (handle){
+
+		curl_easy_setopt(handle,CURLOPT_URL,"http://rednaks.alwaysdata.net/translate/?text=salut");
+		curl_easy_perform(handle);
+		curl_easy_cleanup(handle);
+
+	}
+	//printf("le texte est :%s ",s);
+	else 
+		printf("erreur lors de l'initialisation de la fonction curl_easy_init()\n");
+	
+	
 }
