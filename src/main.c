ebd15463527be5e267d08e2dd635728b2fb18c77
Zaafouri Amine <zaafouri.amine@gmail.com>
2012-06-21 12:47:29 +0100

    accept the text as an argument

--- src/main.c
+++ src/main.c
@@ -2,12 +2,17 @@
 
 int main(int argc,char *argv[]) {
 
-	char *text=NULL;
-	text = (char *)malloc(sizeof(char)*100);
-	printf("Give a text to translate : \n");
-	getLine(text);
-	translate(text);
-	free(text);
+	if (argc !=1) {
+		translate(argv[1]);
+	}else
+	{
+		char *text=NULL;
+		text = (char *)malloc(sizeof(char)*100);
+		printf("Give a text to translate : \n");
+		getLine(text);
+		translate(text);
+		free(text);
+	}
 	return 0;
 
 }
